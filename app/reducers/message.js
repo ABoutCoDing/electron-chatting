e517ccf944f42e30610667d05a87a29508119497
Kesus Kim <kesuskim@gmail.com>
2017-03-30 02:07:52 +0900

    Update README, and refactoring code

--- app/reducers/message.js
+++ app/reducers/message.js
@@ -1,11 +1,9 @@
-import { MSG_UPDATE, MSG_SEND } from '../actions/message';
+import { MSG_UPDATE } from '../actions/message';
 
 export default function message(state = [], action) {
   switch (action.type) {
     case MSG_UPDATE:
       return action.messages;
-    case MSG_SEND:
-      return action.data;
     default:
       return state;
   }
